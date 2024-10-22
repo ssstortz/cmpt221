dcda7f678501378baf65e04e91380b541935f5d2
ssstortz <152337777+ssstortz@users.noreply.github.com>
2024-10-22 16:29:35 -0400

    attempted lab 6

--- labs/lab-6/app.py
+++ labs/lab-6/app.py
@@ -12,15 +12,29 @@ from db.schema.user import User
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
+   # if request.method == 'POST':
+    #    query = f"""INSERT INTO "Users" ("FirstName", "LastName", "Email", "PhoneNumber", "Password")
+  #      VALUES ('{request.form["FirstName"]}',
+   #             '{request.form["LastName"]}',
+  #              '{request.form["Email"]}',
+   #             '{request.form["PhoneNumber"]}',
+   #             '{request.form["Password"]}',
+   #             );"""
+   
+        query = insert(User).values(request.form)
+        with app.app_context():
+            db.session.execute(query)
+            db.session.commit()
 
-@app.route('/login')
+        return render_template('signup.html')
+
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/users')
+@app.route('/users', methods=['GET','POST'])
 def users():
     with app.app_context():
         # select users where the first name is Calista
